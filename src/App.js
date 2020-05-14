5aee49a2a1cb8966b8807d9a8d57691b3a1ebcf4
Zoran Markovic <zoran.markovic@netcentric.biz>
2020-05-14 12:28:41 +0200

    react-starter-kit intial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+
+import Users from './containers/Users';
+import asyncComponent from './hoc/asyncComponent';
+
+// lazy loading is supported automatically by webpack
+const AsyncPizza = asyncComponent(() => {
+    return import('./containers/Pizza.js');
+});
+
+class App extends Component {
+    render () {
+        return (
+            <div>
+                <div>
+                    <Link to="/">Users</Link> |
+                    <Link to="/pizza">Pizza</Link>
+                </div>
+                <div>
+                    <Route path="/" exact component={Users} />
+                    <Route path="/pizza" component={AsyncPizza} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
